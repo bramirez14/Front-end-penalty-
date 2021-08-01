6ccbc63257bb8ace49fe945283c44e3315a1fde0
Administrador <administrador@penaltyarg.com.ar>
2021-08-01 20:37:06 -0300

    ok

--- src/ComponentsGerentes/AprobacionAntcipoSueldo.js
+++ src/ComponentsGerentes/AprobacionAntcipoSueldo.js
@@ -248,11 +248,11 @@ export const AprobacionAntcipoSueldo = () => {
         const color = () => {
           switch (file.estado) {
             case "pendiente":
-              return <h6 style={{ color: "yellow" }}> pendiente...</h6>;
+              return <span style={{ color: "yellow" }}> pendiente...</span>;
             case "aprobado":
-              return <h6 style={{ color: "green" }}> aprobado </h6>;
+              return <span style={{ color: "green" }}> aprobado </span>;
             default:
-              return <h6 style={{ color: "red" }}> rechazado </h6>;
+              return <span style={{ color: "red" }}> rechazado </span>;
           }
         };
         return <>{color()}</>;
@@ -268,11 +268,11 @@ export const AprobacionAntcipoSueldo = () => {
         const color = () => {
           switch (file.estadoFinal) {
             case "pendiente":
-              return <h6 style={{ color: "yellow" }}> pendiente...</h6>;
+              return <span style={{ color: "yellow" }}> pendiente...</span>;
             case "aprobado":
-              return <h6 style={{ color: "green" }}> aprobado </h6>;
+              return <span style={{ color: "green" }}> aprobado </span>;
             default:
-              return <h6 style={{ color: "red" }}> rechazado </h6>;
+              return <span style={{ color: "red" }}> rechazado </span>;
           }
         };
         return <> { N=== '902' && color()}</>;
