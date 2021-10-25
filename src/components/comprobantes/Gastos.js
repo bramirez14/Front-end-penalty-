bfe1ceb182518c17287f6c1f5ded349859a78c61
Bryan Ramirez <bramirez@penaltyarg.com.ar>
2021-10-25 10:11:06 -0300

    oki doki

--- src/components/comprobantes/Gastos.js
+++ src/components/comprobantes/Gastos.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
 import {
   Drawer,
   List,
@@ -12,6 +11,7 @@ import {
 
 import { PdfoImg } from "../../helpers/PdfoImg";
 import { numeroConComa } from "../../helpers/funcioneshelpers";
+import { PeticionGET } from "../config/PeticionGET";
 const { Title } = Typography;
 export const Gastos = () => {
   const [gasto, setGasto] = useState([]);
@@ -21,13 +21,13 @@ export const Gastos = () => {
     setVisible(true);
   };
   const onClose = () => setVisible(false);
-  const GET_gastos = useSelector((state) => state.peticiones_GET.gastos); //viene de  redux
-
+  const GET_gastos= PeticionGET('/gastos')
+  
   return (
     <>
       <List
-        style={{ margin: "auto", width: 700 }}
-        dataSource={GET_gastos}
+        style={{ margin: "auto", width: 700, backgroundColor:'#ffff',borderRadius:20, }}
+        dataSource={GET_gastos?.reverse()}
         bordered
         renderItem={(item) => (
           <List.Item
