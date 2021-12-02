282cebe2bc2b5d62f181b75c8f9dedf6eff44c15
Administrador <administrador@penaltyarg.com.ar>
2021-12-02 16:12:37 -0300

    ok

--- src/redux/actions/rendicionAction.js
+++ src/redux/actions/rendicionAction.js
@@ -1,42 +1,34 @@
 import { axiosURL } from "../../config/axiosURL";
 import { types } from "../types/type";
 
-
-export const getTarjetaCredito=() => async (dispatch)=>{
-    try {
-        const response = await axiosURL.get('/tarjeta/credito');
-        const datos = response.data 
-        dispatch({ type:types.todastc, payload: datos });
-
-    } catch (e) {
-        dispatch({ type:'error',error:e.message})
-    }
-}
-export const  tarjetaCredito=(values,history)=> async (dispatch,getState)=>{
+export const getTarjetaCredito = () => async (dispatch) => {
+  try {
+    const response = await axiosURL.get("/tarjeta/credito");
+    const datos = response.data;
+    dispatch({ type: types.todastc, payload: datos });
+  } catch (e) {
+    dispatch({ type: "error", error: e.message });
+  }
+};
+export const tarjetaCredito =
+  (values, history) => async (dispatch, getState) => {
     const datosUsuario = getState().peticiones_GET.usuario;
     try {
-        let f = new FormData();
-        f.append("file", values.file[0].originFileObj);
-        f.append("nombre", datosUsuario.nombre);
-        f.append("apellido", datosUsuario.apellido);
-        f.append('tarjeta',values.tarjeta);
-        f.append('importe',values.importe);
-        f.append('nota',values.nota)
-        const response = await axiosURL.post('/tarjeta/credito',f);
-        const data= response.data;
-        console.log(data)
-        dispatch({ type:types.tarjeta_credito,payload: data})
-            if (response.data.status === 200) {
-<<<<<<< HEAD
-          history.push("/perfil");
-=======
-         history.push("/perfil");
->>>>>>> f9970d4b884251bc92994b2a5f3e8dd412ffe8af
-        }
-     
-        
-    } 
-    catch (e) {
-    dispatch({ type:'error',error:e.message})
-}
-}
+      let f = new FormData();
+      f.append("file", values.file[0].originFileObj);
+      f.append("nombre", datosUsuario.nombre);
+      f.append("apellido", datosUsuario.apellido);
+      f.append("tarjeta", values.tarjeta);
+      f.append("importe", values.importe);
+      f.append("nota", values.nota);
+      const response = await axiosURL.post("/tarjeta/credito", f);
+      const data = response.data;
+      console.log(data);
+      dispatch({ type: types.tarjeta_credito, payload: data });
+      if (response.data.status === 200) {
+        history.push("/perfil");
+      }
+    } catch (e) {
+      dispatch({ type: "error", error: e.message });
+    }
+  };
