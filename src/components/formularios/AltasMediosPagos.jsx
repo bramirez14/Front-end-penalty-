87db244f1332db6cd4df0a476d4b0835c4e76196
bramirez14 <b.ramireeez@gmail.com>
2022-06-28 22:19:36 -0300

    medio de pago

--- src/components/formularios/AltasMediosPagos.jsx
+++ src/components/formularios/AltasMediosPagos.jsx
@@ -1,13 +1,21 @@
 
 import { CreditCardOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import { Button, Divider, Form, Input } from 'antd';
 import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 import { axiosURL } from '../../config/axiosURL';
+import { Titulo } from '../titulos/Titulo';
 
 export const AltasMediosPagos= () => {
   const [form] = Form.useForm();
   const [, forceUpdate] = useState({}); // To disable submit button at the beginning.
-
+  const [fields, setFields] = useState([
+    {
+      name: ['pago'],
+      value: '',
+    },
+  
+  ]);
   useEffect(() => {
     forceUpdate({});
   }, []);
@@ -16,10 +24,24 @@ export const AltasMediosPagos= () => {
     console.log('Finish:', values);
     const res = await axiosURL.post('/alta/medios/pagos',values)
     console.log(res);
+    if(res.data.status === 200){
+      Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Se agrego con exito!!!',
+      showConfirmButton: false,
+      timer: 2000
+    })
+setFields([{
+  name:['pago'],
+  value:''
+}])
+    }
+    
   };
-
   return (
     <Form 
+    fields={fields}
     className='container-form'
     form={form}  onFinish={onFinish} labelCol={{
         span: 5,
@@ -27,6 +49,8 @@ export const AltasMediosPagos= () => {
       wrapperCol={{
         span: 16,
       }}>
+          <Titulo titulo="Agregar Medio de Pago" />
+        <Divider/>
       <Form.Item
         name="pago"
         rules={[
@@ -38,7 +62,7 @@ export const AltasMediosPagos= () => {
       >
         <Input prefix={<CreditCardOutlined  className="site-form-item-icon" />} placeholder="Username" />
       </Form.Item>
-      
+    
       <Form.Item shouldUpdate>
         {() => (
           <Button
@@ -49,7 +73,7 @@ export const AltasMediosPagos= () => {
               !!form.getFieldsError().filter(({ errors }) => errors.length).length
             }
           >
-            Log in
+            Guardar
           </Button>
         )}
       </Form.Item>
