af0556cdd2f89798cfe403f217609b305536cf88
BME <b.ramireeez@gmail.com>
2022-11-01 00:17:57 -0300

    cambios rendicion

--- src/components/rendiciones/ListaRendiciones.js
+++ src/components/rendiciones/ListaRendiciones.js
@@ -2,26 +2,26 @@ import React, { useContext } from "react";
 import { Encabezado } from "./Encabezado";
 import { Button, Col, Row } from "antd";
 import { CardRendiciones } from "./CardRendiciones";
-import { Link,useNavigate,useParams } from "react-router-dom";
-import {axiosURL} from "../../config/axiosURL";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { axiosURL } from "../../config/axiosURL";
 import { SubEncabezado } from "./SubEncabezado";
 import { UserContext } from "../../context/UserContext";
 import { saveAs } from "file-saver";
 import { useGet } from "../../hooks/useGet";
-import { PeticionGET } from "../../config/PeticionGET";
-import { SocketContext } from "../../context/SocketContext";
+//import { PeticionGET } from "../../config/PeticionGET";
+//import { SocketContext } from "../../context/SocketContext";
 
 export const ListaRendiciones = () => {
   const navigate = useNavigate();
-  const {socket} = useContext(SocketContext)
+  //const { socket } = useContext(SocketContext);
   const { id } = useParams();
-  const [peticionGastoId,axiosGet] = useGet(`/gastos/${id}`);
-  const uid = localStorage.getItem('uid')
-  const datosUsuario = PeticionGET(`/${uid}`)
-  const usuarios= PeticionGET(`/allusers`)
-  const filtroUsuario905= usuarios.filter(u=>u.nvendedor === '905')
+  const [peticionGastoId, axiosGet] = useGet(`/gastos/${id}`);
+  //const uid = localStorage.getItem("uid");
+  //const datosUsuario = PeticionGET(`/${uid}`);
+  //const usuarios = PeticionGET(`/allusers`);
+  //const filtroUsuario905 = usuarios.filter((u) => u.nvendedor === "905");
 
-  const filtrodata905 = filtroUsuario905.map(f=> 
+  /*  const filtrodata905 = filtroUsuario905.map(f=> 
   {return{
   alerta: 'solicitud aprobada',
  info:'Tenes una operacion de  Gasto ',
@@ -32,10 +32,10 @@ export const ListaRendiciones = () => {
  estado:'activa',
  receptor:f.email,
   emisor:datosUsuario.email,
- }});
+ }}); */
 
   // prohibe ingreso por medio de la ruta
-  peticionGastoId?.listo==='Si'&& navigate('/perfil')
+  peticionGastoId?.listo === "Si" && navigate("/perfil");
 
   const todasLasRendicones = peticionGastoId?.rendicion;
   const sumaGastos = todasLasRendicones?.map((sg) => sg.importe);
@@ -46,7 +46,7 @@ export const ListaRendiciones = () => {
     });
   }
   const importe = peticionGastoId?.importe;
-  const Text = useContext(UserContext);
+  //const Text = useContext(UserContext);
 
   const handleClick = async () => {
     let res = await axiosURL.post("/generar/pdf", todasLasRendicones);
@@ -56,44 +56,42 @@ export const ListaRendiciones = () => {
       saveAs(pdfBlob, "penaltyIntranet.pdf");
     }
   };
-  
+
   const onClick = () => {
-    if(peticionGastoId?.sinAnticipo!=='sin'){
-    
+    if (peticionGastoId?.sinAnticipo !== "sin") {
       navigate(`/crear/rendicion/${id}`);
-  }else{
-    navigate(`/crear/rendicion/${id}`);
-  }
+    } else {
+      navigate(`/crear/rendicion/${id}`);
+    }
   };
 
-  const listo = async ()=>{
-    const obj={
-    alerta: 'Rendiciones finalizadas',
-    info:'Tenes una operacion de  Gasto ',
-    nombre:`${datosUsuario.nombre} ${datosUsuario.apellido}`,
-    usuarioId:uid,
-    f: new Date().toLocaleString(),
-    path:'/aprobacion/gastos',
-    estado:'activa',
-    receptor:datosUsuario.gerente.email,
-     emisor:datosUsuario.email,
-    }
-   
-    if(peticionGastoId?.sinAnticipo!=='sin'){
-      for (const i of filtrodata905) {
-      socket.emit('alerta-nueva',i)
-      }
-      
-    }else{
-      socket.emit('alerta-nueva',obj)
+  const listo = async () => {
+    // const obj={
+    // alerta: 'Rendiciones finalizadas',
+    // info:'Tenes una operacion de  Gasto ',
+    // nombre:`${datosUsuario.nombre} ${datosUsuario.apellido}`,
+    // usuarioId:uid,
+    // f: new Date().toLocaleString(),
+    // path:'/aprobacion/gastos',
+    // estado:'activa',
+    // receptor:datosUsuario.gerente.email,
+    //  emisor:datosUsuario.email,
+    // }
 
-    }
-      let res = await axiosURL.put(`/gasto/finalizado/${id}`,{listo:'Si'});
-      res.status===200&& navigate('/gastos')
+    // if(peticionGastoId?.sinAnticipo!=='sin'){
+    //   for (const i of filtrodata905) {
+    //   socket.emit('alerta-nueva',i)
+    //   }
+
+    // }else{
+    //   socket.emit('alerta-nueva',obj)
+
+    // }
+    let res = await axiosURL.put(`/gasto/finalizado/${id}`, { listo: "Si" });
+    res.status === 200 && navigate("/gastos");
   };
   return (
-    <div className='container-form'>
-
+    <div className="container-form">
       <Encabezado />
       <SubEncabezado
         uuid={id}
@@ -102,7 +100,6 @@ export const ListaRendiciones = () => {
         sinAnticipo={peticionGastoId.sinAnticipo}
       />
       <Row>
-      
         <Col xs={24} sm={24} md={24} lg={24} xl={24}>
           <Button style={{ marginTop: "10px" }} onClick={onClick}>
             Agregar Gasto
@@ -110,30 +107,28 @@ export const ListaRendiciones = () => {
           <Link to="/gastos">
             <Button className="btn-list-rendicion">X</Button>
           </Link>
-            <CardRendiciones
-             
-              data={todasLasRendicones}
-              uid={id}
-              importeAnt={totalDeImporte}
-              axiosGet={axiosGet}
-            />
+          <CardRendiciones
+            data={todasLasRendicones}
+            uid={id}
+            importeAnt={totalDeImporte}
+            axiosGet={axiosGet}
+          />
         </Col>
-        </Row>
-        <Row>
-        <Col >
-          
-          <Button  style={{ marginTop: "10px" }} onClick={handleClick}>
-          Generar PDF
+      </Row>
+      <Row>
+        <Col>
+          <Button style={{ marginTop: "10px" }} onClick={handleClick}>
+            Generar PDF
           </Button>
-        
-      
-          <Button  style={{ marginTop: "10px",marginLeft:'10px'}} onClick={listo}>
-          Finalizar
+
+          <Button
+            style={{ marginTop: "10px", marginLeft: "10px" }}
+            onClick={listo}
+          >
+            Finalizar
           </Button>
-        
-        
         </Col>
-        </Row>
+      </Row>
     </div>
   );
 };
